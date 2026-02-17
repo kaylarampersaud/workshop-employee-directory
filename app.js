3160cd35a7c536ede574588623fa2e2635583c63
Kayla Rampersaud <krampersaud98@gmail.com>
2026-02-17 12:46:53 -0500

    added GET endpoint

--- app.js
+++ app.js
@@ -3,3 +3,9 @@ const app = express();
 export default app;
 
 import employees from "#db/employees";
+
+// GET / endpoint
+// GET / sends the string "Hello employees!".
+app.get("/", (req, res) => {
+  res.send("Hello employees!");
+});
